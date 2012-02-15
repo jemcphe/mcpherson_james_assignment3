b3230f597c3614fd75f41a334022fba556bb59ba
James McPherson <jemcphe@fullsail.edu>
2012-02-14 23:51:11 -0600

    New Function playerInfo
    Function will output player information such as player name, position,
    and number.

--- mcpherson_james_assignment3.js
+++ mcpherson_james_assignment3.js
@@ -1,3 +1,10 @@
 alert("JavaScript works!");
 
-console.log(json);
+var playerInfo = function (json) {
+	for( var i = 0; i < json.players.length; i++){
+		var player = json.players[i];
+		console.log("Player: " + player.name + ", Position: " + player.position + ", Number: " + player.number);
+	};
+};
+
+playerInfo(json);
