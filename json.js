77d23469959bb6a182597bd4e42a4a38a440f685
James McPherson <jemcphe@fullsail.edu>
2012-02-14 23:20:37 -0600

    json objects
    Created json data for players.  This is player data for each member of
    the Oklahoma City Thunder

--- json.js
+++ json.js
@@ -1,2 +1,81 @@
 // JSON Object File
 
+var json = {
+	"players" : [
+		{
+			"name" : "Russel Westbrook",
+			"position" : "Guard",
+			"number" : 0
+		},
+		{
+			"name" : "Cole Aldrich",
+			"position" : "Center",
+			"number" : 45
+		},
+		{
+			"name" : "Nick Collison",
+			"position" : "Forward, Center",
+			"number" : 4
+		},
+		{
+			"name" : "Daequan Cook",
+			"position" : "Forward, Guard",
+			"number" : 14
+		},
+		{
+			"name" : "Kevin Durant",
+			"position" : "Forward",
+			"number" : 35
+		},
+		{
+			"name" : "James Harden",
+			"position" : "Guard",
+			"number" : 13
+		},
+		{
+			"name" : "Lazar Hayward",
+			"position" : "Forward",
+			"number" : 11
+		},
+		{
+			"name" : "Serge Ibaka",
+			"position" : "Forward, Center",
+			"number" : 9
+		},
+		{
+			"name" : "Royal Ivey",
+			"position" : "Guard",
+			"number" : 7
+		},
+		{
+			"name" : "Reggie Jackson",
+			"position" : "Guard",
+			"number" : 15
+		},
+		{
+			"name" : "Eric Maynor",
+			"position" : "Guard",
+			"number" : 6
+		},
+		{
+			"name" : "Nazr Mohammed",
+			"position" : "Center",
+			"number" : 8
+		},
+		{
+			"name" : "Kendrick Perkins",
+			"position" : "Center",
+			"number" : 5
+		},
+		{
+			"name" : "Ryan Reid",
+			"position" : "Forward",
+			"number" : 33
+		},
+		{
+			"name" : "Thabo Sefolosha",
+			"position" : "Guard",
+			"number" : 2
+		},
+	]
+}
