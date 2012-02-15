7ecd411ff6e6bd6c3f13b65c744746cfc78cc1c7
James McPherson <jemcphe@fullsail.edu>
2012-02-14 23:50:18 -0600

    Added new key : value pair
    Added "starter : true / false" pair.  I will use this later for a
    function that will get starters.

--- json.js
+++ json.js
@@ -5,77 +5,92 @@ var json = {
 		{
 			"name" : "Russel Westbrook",
 			"position" : "Guard",
-			"number" : 0
+			"number" : 0,
+			"starter" : true
 		},
 		{
 			"name" : "Cole Aldrich",
 			"position" : "Center",
-			"number" : 45
+			"number" : 45,
+			"starter" : false
 		},
 		{
 			"name" : "Nick Collison",
 			"position" : "Forward, Center",
-			"number" : 4
+			"number" : 4,
+			"starter" : false
 		},
 		{
 			"name" : "Daequan Cook",
 			"position" : "Forward, Guard",
-			"number" : 14
+			"number" : 14,
+			"starter" : true
 		},
 		{
 			"name" : "Kevin Durant",
 			"position" : "Forward",
-			"number" : 35
+			"number" : 35,
+			"starter" : true
 		},
 		{
 			"name" : "James Harden",
 			"position" : "Guard",
-			"number" : 13
+			"number" : 13,
+			"starter" : false
 		},
 		{
 			"name" : "Lazar Hayward",
 			"position" : "Forward",
-			"number" : 11
+			"number" : 11,
+			"starter" : false
 		},
 		{
 			"name" : "Serge Ibaka",
 			"position" : "Forward, Center",
-			"number" : 9
+			"number" : 9,
+			"starter" : true
 		},
 		{
 			"name" : "Royal Ivey",
 			"position" : "Guard",
-			"number" : 7
+			"number" : 7,
+			"starter" : false
 		},
 		{
 			"name" : "Reggie Jackson",
 			"position" : "Guard",
-			"number" : 15
+			"number" : 15,
+			"starter" : false
 		},
 		{
 			"name" : "Eric Maynor",
 			"position" : "Guard",
-			"number" : 6
+			"number" : 6,
+			"starter" : false
 		},
 		{
 			"name" : "Nazr Mohammed",
 			"position" : "Center",
-			"number" : 8
+			"number" : 8,
+			"starter" : false
 		},
 		{
 			"name" : "Kendrick Perkins",
 			"position" : "Center",
-			"number" : 5
+			"number" : 5,
+			"starter" : true
 		},
 		{
 			"name" : "Ryan Reid",
 			"position" : "Forward",
-			"number" : 33
+			"number" : 33,
+			"starter" : false
 		},
 		{
 			"name" : "Thabo Sefolosha",
 			"position" : "Guard",
-			"number" : 2
+			"number" : 2,
+			"starter" : false
 		},
 	]
 }
